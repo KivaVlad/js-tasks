fabb83cbeaf6f9af75c79430ff040f3b7020fd8e
Vladislav <melnikovv717@gmail.com>
2024-02-25 22:00:56 +0300

    reload exercise-04

--- exercise-04/exercise-04.js
+++ exercise-04/exercise-04.js
@@ -1,30 +1,20 @@
 function transformWords(num, words) {  
-	let n = num % 10;
+    let n = Math.abs(num);
+  
+    n %= 100;
+    if (n >= 5 && n <= 20) return `${num} ${words[2]}`;
 
-	if (num > 10 && num < 20) {
-        return `${num} ${words[2]}`;
-    }
-
-	if (n > 1 && n < 5) {
-        return `${num} ${words[1]}`;
-    }
-
-	if (n === 1) {
-        return `${num} ${words[0]}`;
-    } 
-
+    n %= 10;
+    if (n === 1) return `${num} ${words[0]}`;
+    if (n >= 2 && n <= 4) return `${num} ${words[1]}`
+    
 	return `${num} ${words[2]}`;
 }
-
-
-// Функция transformWords принимает число и массив из 3-х возможных склонений слов. 
-// Создаем переменную n, которая вычисляет остаток от деления на 10 входящего числа num (это нужно для этапов проверки склонения).
-// Далее делаем проверки и возвращаем входящее число и правильное окончание, в зависимости от склонения
 
 const users = ['пользователь', 'пользователя', 'пользователей'];
 const messages = ['сообщение', 'сообщения', 'сообщений'];
 
-console.log(transformWords(112, messages)); // 112 сообщения
+console.log(transformWords(112, messages)); // 112 сообщений
 console.log(transformWords(12, messages)); // 12 сообщений
 console.log(transformWords(1, messages)); // 1 сообщение
 console.log(transformWords(1024, users)); // 1024 пользователя
